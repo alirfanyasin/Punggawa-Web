265037e20876372ade950036972c42e4ec768deb
alirfanyasin <risayunita21@gmail.com>
2023-08-27 18:21:48 +0530

    perf: create template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Home')
 
 @section('content')
+    {{-- Ini adalah sebuah contoh penggunaan template --}}
     {{-- Header start --}}
     <h1 class="text-3xl text-red-900 font-bold underline">
         Hello world!
