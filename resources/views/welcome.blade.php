eed769031613c9ade94a80060cc687f25a0d510e
alirfanyasin <risayunita21@gmail.com>
2023-08-27 17:22:32 +0530

    Instalation Flowbite (package)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,16 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
     <h1 class="text-3xl text-red-900 font-bold underline">
         Hello world!
     </h1>
+    <div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+        <span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+    </div>
 </body>
 
 </html>
