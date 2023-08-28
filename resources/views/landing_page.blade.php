41f4e777fde106b2fde4dfac7bef91ed81bf9a68
alirfanyasin <risayunita21@gmail.com>
2023-08-28 07:15:36 +0700

    feat: jumbotron and about

---
+++ resources/views/landing_page.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+@section('title', env('APP_NAME') . '- Pusat informasi dan pengelolaan lomba di bidang akademik dan non-akademik
+  Institut Teknologi Telkom Surabaya')
+@section('content')
+  <!-- Jumbotron start -->
+  <section id="jumbotron-section">
+    <div class="flex justify-center h-screen">
+      <div class="container flex items-center max-w-screen-xl mt-10">
+        <div class="w-6/12 tagline">
+          <h1 class="font-semibold text-white uppercase text-7xl">Punggawa
+            <br>
+            <span class="text-[#FFB902]">Inspiratif</span>
+          </h1>
+          <p class="mt-10 text-[#E9E9E9] text-xl">Pusat informasi dan pengelolaan lomba di bidang akademik dan non-akademik
+            Institut Teknologi Telkom Surabaya</p>
+
+          <a href="#" class="inline-block px-7 py-3 mt-10 rounded-lg bg-white text-[#15616D] shadow-lg">Lihat
+            Lomba</a>
+        </div>
+        <div class="w-6/12 mx-auto tagline">
+          <img src="/assets/image/object-jp.png" class="w-full" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Jumbotron end -->
+
+
+  <!-- About start -->
+  <section class="mt-32">
+    <header class="text-center">
+      <h2 class="text-4xl font-semibold text-[#3D4650]">Tentang Punggawa
+        <span class="text-[#15616D]">Inspiratif</span>
+      </h2>
+    </header>
+    <div class="flex justify-center mt-16 rounded-lg">
+      <iframe width="700" height="400" src="https://www.youtube.com/embed/ck-x84uSgxg?si=VqKhVarxivwhhPIy"
+        title="YouTube video player" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen class="rounded-2xl"></iframe>
+    </div>
+  </section>
+  <!-- About end -->
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,40 +2,41 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description"
-        content="Punggawa adalah sebuah organisasi untuk menampung lomba-lomba untuk mahasiswa IT Telkom Surabaya.">
-    <meta name="keywords" content="Punggawa, Punggawa IT Telkom Surabaya, Lomba-Lomba Mahasiswa">
-    <meta name="robots" content="index,follow">
-    <link rel="canonical" href="https://www.example.com/index">
-    <meta name="author" content="Punggawa Development Team ">
-    <meta property="og:title" content="Punggawa Inspiratif - IT Telkom Surabaya">
-    <meta property="og:description"
-        content="Punggawa adalah sebuah organisasi untuk menampung lomba-lomba untuk mahasiswa IT Telkom Surabaya.">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="description"
+    content="Punggawa adalah sebuah organisasi untuk menampung lomba-lomba untuk mahasiswa IT Telkom Surabaya.">
+  <meta name="keywords" content="Punggawa, Punggawa IT Telkom Surabaya, Lomba-Lomba Mahasiswa">
+  <meta name="robots" content="index,follow">
+  <link rel="canonical" href="https://www.example.com/index">
+  <meta name="author" content="Punggawa Development Team ">
+  <meta property="og:title" content="Punggawa Inspiratif - IT Telkom Surabaya">
+  <meta property="og:description"
+    content="Punggawa adalah sebuah organisasi untuk menampung lomba-lomba untuk mahasiswa IT Telkom Surabaya.">
 
-    <title>@yield('title')</title>
-    {{-- Style CSS | Tailwind CSS and Flowbite --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    @push('css-custom')
-    @endpush
+  <title>@yield('title')</title>
+  {{-- Style CSS | Tailwind CSS and Flowbite --}}
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  {{-- CSS Custom --}}
+  <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+  @push('css-custom')
+  @endpush
 </head>
 
 <body>
 
-    @include('components.navbar')
+  @include('components.navbar')
 
-    <main>
-        @yield('content')
-    </main>
+  <main>
+    @yield('content')
+  </main>
 
-    @include('components.footer')
+  @include('components.footer')
 
 
-    @push('js-custom')
-    @endpush
+  @push('js-custom')
+  @endpush
 </body>
 
 </html>
