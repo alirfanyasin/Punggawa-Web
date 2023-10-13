f934415fac3dd0808a47c0c4ea1c21925ae4abe7
alirfanyasin <risayunita21@gmail.com>
2023-10-13 10:44:40 +0700

    feat: create contest page in role division

---
+++ resources/views/app/division/gemastik/contest.blade.php
@@ -0,0 +1,130 @@
+@extends('layouts.app')
+@section('title', env('APP_NAME') . ' - Contest')
+@section('content')
+  {{-- Breadcrumb start --}}
+  <div class="flex justify-between breadcrumb">
+    <h3 class="font-semibold lg:text-2xl xs:text-md md:text-xl">Contest</h3>
+    <nav class="flex" aria-label="Breadcrumb">
+      <ol class="inline-flex items-center space-x-1 md:space-x-3">
+        <li class="inline-flex items-center">
+          <a href="#"
+            class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+            <svg class="w-3 h-3 mr-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+              viewBox="0 0 20 20">
+              <path
+                d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+            </svg>
+            Home
+          </a>
+        </li>
+        <li>
+          <div class="flex items-center">
+            <svg class="w-3 h-3 mx-1 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+              viewBox="0 0 6 10">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="m1 9 4-4-4-4" />
+            </svg>
+            <a href="{{ route('division.contest') }}"
+              class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">Contest
+            </a>
+          </div>
+        </li>
+      </ol>
+    </nav>
+  </div>
+  {{-- Breadcrumb end --}}
+
+  {{-- Wrapper start --}}
+  <section id="wrapper" class="h-screen mt-8">
+    <div class="">
+      <div class="flex justify-between">
+        <div class="mb-10 tab-button">
+          <a href="#" class="px-8 py-2 border rounded-full border-2-outline bg-slate-200 hover:bg-slate-200"
+            id="btn-all">Active</a>
+          <a href="#" class="px-8 py-2 border rounded-full border-2-outline hover:bg-slate-200"
+            id="btn-finish">Done</a>
+        </div>
+        <div>
+          <a href="{{ route('division.contest.create') }}"
+            class="px-8 py-2 rounded-full text-white bg-[#15616D] hover:bg-[#2a8996]" id="btn-all">Add
+            Contest</a>
+        </div>
+      </div>
+
+      <div id="tab1">
+        <div class="grid gap-4 lg:grid-cols-4 md:grid-cols-2 xs:grid-cols-1">
+          <div class="bg-[#F4F2F2] p-5 rounded-xl relative">
+            <div class="w-full h-32 overflow-hidden rounded-xl">
+              <img src="{{ asset('assets/image/card-img.jpeg') }}" class="w-full" alt="">
+            </div>
+            <div class="mt-5">
+              <h4 class="font-semibold">Lomba Competitive Programming Gemastik 2023</h4>
+              <div class="flex items-center justify-between mt-7">
+                <span class="px-3 py-1 text-xs text-white rounded-full bg-[#15616D]">Active</span>
+                <span class="text-sm font-light">02 Januari 2023</span>
+              </div>
+            </div>
+          </div>
+          <div class="bg-[#F4F2F2] p-5 rounded-xl relative">
+            <div class="w-full h-32 overflow-hidden rounded-xl">
+              <img src="{{ asset('assets/image/card-img.jpeg') }}" class="w-full" alt="">
+            </div>
+            <div class="mt-5">
+              <h4 class="font-semibold">Lomba Competitive Programming Gemastik 2023</h4>
+              <div class="flex items-center justify-between mt-7">
+                <span class="px-3 py-1 text-xs text-white rounded-full bg-[#15616D]">Active</span>
+                <span class="text-sm font-light">02 Januari 2023</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="tab2" hidden>
+        <div class="grid gap-4 lg:grid-cols-4 md:grid-cols-2 xs:grid-cols-1">
+          <div class="bg-[#F4F2F2] p-5 rounded-xl relative">
+            <div class="w-full h-32 overflow-hidden rounded-xl">
+              <img src="{{ asset('assets/image/card-img.jpeg') }}" class="w-full" alt="">
+            </div>
+            <div class="mt-5">
+              <h4 class="font-semibold">Lomba Competitive Programming Gemastik 2023</h4>
+              <div class="flex items-center justify-between mt-7">
+                <span class="px-3 py-1 text-xs text-white bg-red-600 rounded-full">Done</span>
+                <span class="text-sm font-light">02 Januari 2023</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+  {{-- Wrapper end --}}
+
+  @include('components.app.footer')
+
+  @push('js-custom')
+    <script>
+      var btnAll = document.querySelector('#btn-all');
+      var btnFinish = document.querySelector('#btn-finish');
+      var tab1 = document.querySelector('#tab1');
+      var tab2 = document.querySelector('#tab2');
+
+
+      btnAll.addEventListener('click', function(e) {
+        e.preventDefault()
+        this.classList.add('bg-slate-200')
+        btnFinish.classList.remove('bg-slate-200')
+        tab1.removeAttribute('hidden');
+        tab2.setAttribute('hidden', 'true');
+      })
+
+      btnFinish.addEventListener('click', function(e) {
+        e.preventDefault()
+        this.classList.add('bg-slate-200')
+        btnAll.classList.remove('bg-slate-200')
+        tab1.setAttribute('hidden', 'true');
+        tab2.removeAttribute('hidden');
+      })
+    </script>
+  @endpush
+@endsection
--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -1,59 +1,103 @@
 <aside class="bg-[#F4F2F2] lg:w-2/12 h-screen  lg:block lg:relative xs:hidden xs:w-8/12 xs:absolute z-10">
-  <ul class="pt-20">
-    <li class="text-[#15616D] font-semibold  hover:font-bold mb-3">
-      @if (Request::is('dashboard'))
-        <a href="/dashboard" class="flex items-center px-8 py-3">
-          <img src="{{ asset('assets/image/icon/icon-dashboard-solid.png') }}" class="mr-3 w-7 h-7" alt="">
-          Dashboard</a>
-      @else
-        <a href="/dashboard" class="flex items-center px-8 py-3">
-          <img src="{{ asset('assets/image/icon/icon-dashboard.png') }}" class="mr-3 w-7 h-7" alt="">
-          Dashboard</a>
-      @endif
-    </li>
-    <li class="text-[#15616D] font-semibold  hover:font-bold mb-3">
-      @if (Request::is('contest-followed'))
-        <a href="/contest-followed" class="flex items-center px-8 py-3">
-          <img src="{{ asset('assets/image/icon/icon-trophy-solid.png') }}" class="mr-3 w-7 h-7" alt="">Contest
-          Followed</a>
-      @else
-        <a href="/contest-followed" class="flex items-center px-8 py-3">
-          <img src="{{ asset('assets/image/icon/icon-trophy.png') }}" class="mr-3 w-7 h-7" alt="">Contest
-          Followed</a>
-      @endif
-    </li>
-    <li class="text-[#15616D] font-semibold  hover:font-bold mb-3">
-      @if (Request::is('bootcamp'))
-        <a href="/bootcamp" class="flex items-center px-8 py-3">
-          <img src="{{ asset('assets/image/icon/icon-bootcamp-solid.png') }}" class="mr-3 w-7 h-7"
-            alt="">Bootcamp</a>
-      @else
-        <a href="/bootcamp" class="flex items-center px-8 py-3">
-          <img src="{{ asset('assets/image/icon/icon-bootcamp.png') }}" class="mr-3 w-7 h-7" alt="">Bootcamp</a>
-      @endif
+  @hasrole('user')
+    <ul class="pt-20">
+      <li class="text-[#15616D] font-semibold  hover:font-bold mb-3">
+        @if (Request::is('dashboard'))
+          <a href="/dashboard" class="flex items-center px-8 py-3">
+            <img src="{{ asset('assets/image/icon/icon-dashboard-solid.png') }}" class="mr-3 w-7 h-7" alt="">
+            Dashboard</a>
+        @else
+          <a href="/dashboard" class="flex items-center px-8 py-3">
+            <img src="{{ asset('assets/image/icon/icon-dashboard.png') }}" class="mr-3 w-7 h-7" alt="">
+            Dashboard</a>
+        @endif
+      </li>
+      <li class="text-[#15616D] font-semibold  hover:font-bold mb-3">
+        @if (Request::is('contest-followed'))
+          <a href="/contest-followed" class="flex items-center px-8 py-3">
+            <img src="{{ asset('assets/image/icon/icon-trophy-solid.png') }}" class="mr-3 w-7 h-7" alt="">Contest
+            Followed</a>
+        @else
+          <a href="/contest-followed" class="flex items-center px-8 py-3">
+            <img src="{{ asset('assets/image/icon/icon-trophy.png') }}" class="mr-3 w-7 h-7" alt="">Contest
+            Followed</a>
+        @endif
+      </li>
+      <li class="text-[#15616D] font-semibold  hover:font-bold mb-3">
+        @if (Request::is('bootcamp'))
+          <a href="/bootcamp" class="flex items-center px-8 py-3">
+            <img src="{{ asset('assets/image/icon/icon-bootcamp-solid.png') }}" class="mr-3 w-7 h-7"
+              alt="">Bootcamp</a>
+        @else
+          <a href="/bootcamp" class="flex items-center px-8 py-3">
+            <img src="{{ asset('assets/image/icon/icon-bootcamp.png') }}" class="mr-3 w-7 h-7" alt="">Bootcamp</a>
+        @endif
 
-    </li>
-    <li class="text-[#15616D] font-semibold  hover:font-bold mb-3">
-      @if (Request::is('certificate'))
-        <a href="certificate" class="flex items-center px-8 py-3">
-          <img src="{{ asset('assets/image/icon/icon-certificate-solid.png') }}" class="mr-3 w-7 h-7"
-            alt="">Certificate</a>
-      @else
-        <a href="certificate" class="flex items-center px-8 py-3">
-          <img src="{{ asset('assets/image/icon/icon-certificate.png') }}" class="mr-3 w-7 h-7"
-            alt="">Certificate</a>
-      @endif
-    </li>
-    <li class="text-[#15616D] font-semibold  hover:font-bold mb-3">
-      @if (Request::is('setting'))
-        <a href="setting" class="flex items-center px-8 py-3">
-          <img src="{{ asset('assets/image/icon/icon-setting-solid.png') }}" class="mr-3 w-7 h-7"
-            alt="">Setting</a>
-      @else
-        <a href="setting" class="flex items-center px-8 py-3">
-          <img src="{{ asset('assets/image/icon/icon-setting.png') }}" class="mr-3 w-7 h-7" alt="">Setting</a>
-      @endif
+      </li>
+      <li class="text-[#15616D] font-semibold  hover:font-bold mb-3">
+        @if (Request::is('certificate'))
+          <a href="certificate" class="flex items-center px-8 py-3">
+            <img src="{{ asset('assets/image/icon/icon-certificate-solid.png') }}" class="mr-3 w-7 h-7"
+              alt="">Certificate</a>
+        @else
+          <a href="certificate" class="flex items-center px-8 py-3">
+            <img src="{{ asset('assets/image/icon/icon-certificate.png') }}" class="mr-3 w-7 h-7"
+              alt="">Certificate</a>
+        @endif
+      </li>
+      <li class="text-[#15616D] font-semibold  hover:font-bold mb-3">
+        @if (Request::is('setting'))
+          <a href="setting" class="flex items-center px-8 py-3">
+            <img src="{{ asset('assets/image/icon/icon-setting-solid.png') }}" class="mr-3 w-7 h-7"
+              alt="">Setting</a>
+        @else
+          <a href="setting" class="flex items-center px-8 py-3">
+            <img src="{{ asset('assets/image/icon/icon-setting.png') }}" class="mr-3 w-7 h-7" alt="">Setting</a>
+        @endif
 
-    </li>
-  </ul>
+      </li>
+    </ul>
+  @endhasrole
+
+  @hasrole('super-admin')
+    <ul class="pt-20">
+      <li class="text-[#15616D] font-semibold  hover:font-bold mb-3">
+        @if (Request::is('app/dashboard'))
+          <a href="{{ route('super-admin.dashboard') }}" class="flex items-center px-8 py-3">
+            <img src="{{ asset('assets/image/icon/icon-dashboard-solid.png') }}" class="mr-3 w-7 h-7" alt="">
+            Dashboard</a>
+        @else
+          <a href="{{ route('super-admin.dashboard') }}" class="flex items-center px-8 py-3">
+            <img src="{{ asset('assets/image/icon/icon-dashboard.png') }}" class="mr-3 w-7 h-7" alt="">
+            Dashboard</a>
+        @endif
+      </li>
+    </ul>
+  @endhasrole
+
+  @hasrole('division')
+    <ul class="pt-20">
+      <li class="text-[#15616D] font-semibold  hover:font-bold mb-3">
+        @if (Request::is('division/dashboard'))
+          <a href="{{ route('division.dashboard') }}" class="flex items-center px-8 py-3">
+            <img src="{{ asset('assets/image/icon/icon-dashboard-solid.png') }}" class="mr-3 w-7 h-7" alt="">
+            Dashboard</a>
+        @else
+          <a href="{{ route('division.dashboard') }}" class="flex items-center px-8 py-3">
+            <img src="{{ asset('assets/image/icon/icon-dashboard.png') }}" class="mr-3 w-7 h-7" alt="">
+            Dashboard</a>
+        @endif
+      </li>
+      <li class="text-[#15616D] font-semibold  hover:font-bold mb-3">
+        @if (Request::is('division/contest') || Request::is('division/contest/*'))
+          <a href="{{ route('division.contest') }}" class="flex items-center px-8 py-3">
+            <img src="{{ asset('assets/image/icon/icon-trophy-solid.png') }}" class="mr-3 w-7 h-7"
+              alt="">Contest</a>
+        @else
+          <a href="{{ route('division.contest') }}" class="flex items-center px-8 py-3">
+            <img src="{{ asset('assets/image/icon/icon-trophy.png') }}" class="mr-3 w-7 h-7" alt="">Contest</a>
+        @endif
+      </li>
+    </ul>
+  @endhasrole
 </aside>
