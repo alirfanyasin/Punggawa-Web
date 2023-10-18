e6e08efc8ba0724d85c26272c3b77b6b8471e7f7
alirfanyasin <risayunita21@gmail.com>
2023-10-18 15:15:35 +0700

    feat: authorization navbar in all role

--- resources/views/components/app/navbar.blade.php
+++ resources/views/components/app/navbar.blade.php
@@ -21,11 +21,13 @@
       <!-- Dropdown menu -->
       <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
         <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-          <li>
-            <a href="{{ route('account.setting') }}"
-              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"><i
-                class="fa-regular fa-user"></i>&nbsp; Account</a>
-          </li>
+          @hasrole(['super-admin', 'user'])
+            <li>
+              <a href="{{ route('account.setting') }}"
+                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"><i
+                  class="fa-regular fa-user"></i>&nbsp; Account</a>
+            </li>
+          @endhasrole
           {{-- <li>
             <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"><i
                 class="fa-solid fa-gear"></i>&nbsp; Activity Log</a>
